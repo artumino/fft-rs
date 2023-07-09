bccb885e4eb88df23531c81b2b0fc0d28b68913c
Jacopo Libè <jacopo.libe@gmail.com>
2023-07-09 14:27:21 +0200

    Generic benches with hanning window as well

--- benches/bench.rs
+++ benches/bench.rs
@@ -1,4 +1,8 @@
-use std::{sync::Arc};
+use std::{
+    fmt::Debug,
+    ops::{Add, Mul, Sub},
+    sync::Arc,
+};
 
 use criterion::{
     black_box, criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, BenchmarkId,
@@ -6,48 +10,57 @@ use criterion::{
 };
 use fft::{
     allocators::{array::ArrayAllocator, boxed::BoxedAllocator},
-    implementations::CooleyTukey,
-    windows::Rect,
+    implementations::{cooley_tukey::OmegaCalculator, CooleyTukey},
+    windows::{hanning::Hanning, Rect},
     Allocator, Implementation, WindowFunction,
 };
 use num_complex::Complex32;
 use random::Source;
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let mut group = c.benchmark_group("FFT<f32>");
-    run_bench_f32::<1_048_576, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_f32::<65_536, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_f32::<1_024, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_f32::<1_024, CooleyTukey, Rect, ArrayAllocator>(&mut group);
-    run_bench_f32::<512, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_f32::<512, CooleyTukey, Rect, ArrayAllocator>(&mut group);
-    run_bench_f32::<32, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_f32::<32, CooleyTukey, Rect, ArrayAllocator>(&mut group);
-    group.finish();
+    run_cooley_bench_group::<Complex32, Rect>(c, "FFT<Complex32>");
+    run_cooley_bench_group::<f32, Rect>(c, "FFT<f32>");
+    run_cooley_bench_group::<Complex32, Hanning>(c, "FFT<Complex32> + Hanning");
+    run_cooley_bench_group::<f32, Hanning>(c, "FFT<f32> + Hanning");
+}
 
-    let mut group = c.benchmark_group("FFT<Complex32>");
-    run_bench_c32::<1_048_576, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_c32::<65_536, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_c32::<1_024, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_c32::<1_024, CooleyTukey, Rect, ArrayAllocator>(&mut group);
-    run_bench_c32::<512, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_c32::<512, CooleyTukey, Rect, ArrayAllocator>(&mut group);
-    run_bench_c32::<32, CooleyTukey, Rect, BoxedAllocator>(&mut group);
-    run_bench_c32::<32, CooleyTukey, Rect, ArrayAllocator>(&mut group);
+fn run_cooley_bench_group<T, W>(c: &mut Criterion, name: &'static str)
+where
+    W: WindowFunction<T>,
+    T: Copy
+        + Debug
+        + Default
+        + Add<Output = T>
+        + OmegaCalculator<T>
+        + Mul<<T as OmegaCalculator<T>>::TMul, Output = T>
+        + Sub<Output = T>,
+    [T]: Randomizable<T>,
+{
+    let mut group = c.benchmark_group(name);
+    run_bench::<T, 1_048_576, CooleyTukey, W, BoxedAllocator>(&mut group);
+    run_bench::<T, 65_536, CooleyTukey, W, BoxedAllocator>(&mut group);
+    run_bench::<T, 1_024, CooleyTukey, W, BoxedAllocator>(&mut group);
+    run_bench::<T, 1_024, CooleyTukey, W, ArrayAllocator>(&mut group);
+    run_bench::<T, 512, CooleyTukey, W, BoxedAllocator>(&mut group);
+    run_bench::<T, 512, CooleyTukey, W, ArrayAllocator>(&mut group);
+    run_bench::<T, 32, CooleyTukey, W, BoxedAllocator>(&mut group);
+    run_bench::<T, 32, CooleyTukey, W, ArrayAllocator>(&mut group);
     group.finish();
 }
 
-fn run_bench_f32<const N: usize, I, W, A>(c: &mut BenchmarkGroup<'_, WallTime>)
+fn run_bench<T, const N: usize, I, W, A>(c: &mut BenchmarkGroup<'_, WallTime>)
 where
-    I: Implementation<f32, N, A>,
-    A: Allocator<f32, N>,
-    W: WindowFunction<f32>,
+    I: Implementation<T, N, A>,
+    A: Allocator<T, N>,
+    W: WindowFunction<T>,
+    T: Copy + Debug + Default,
+    [T]: Randomizable<T>,
 {
     let allocator_name = std::any::type_name::<A>().split("::").last().unwrap();
     let strategy_name = std::any::type_name::<I>().split("::").last().unwrap();
-    let vec = generate_f32::<N>();
+    let vec = generate::<T, N>();
     let mut out_spec = A::allocate();
-    let boxed_engine = fft::Engine::<f32, N, I, W, A>::new();
+    let boxed_engine = fft::Engine::<T, N, I, W, A>::new();
     c.bench_with_input(
         BenchmarkId::new(format!("{strategy_name}_{allocator_name}").as_str(), N),
         &N,
@@ -55,43 +68,38 @@ where
     );
 }
 
-fn run_bench_c32<const N: usize, I, W, A>(c: &mut BenchmarkGroup<'_, WallTime>)
+pub trait Randomizable<T> {
+    fn randomize(&mut self, seed: u64);
+}
+
+fn generate<T: Default + Copy + Debug, const S: usize>() -> Arc<[T; S]>
 where
-    I: Implementation<Complex32, N, A>,
-    A: Allocator<Complex32, N>,
-    W: WindowFunction<Complex32>,
+    [T]: Randomizable<T>,
 {
-    let allocator_name = std::any::type_name::<A>().split("::").last().unwrap();
-    let strategy_name = std::any::type_name::<I>().split("::").last().unwrap();
-    let vec = generate_c32::<N>();
-    let mut out_spec = A::allocate();
-    let boxed_engine = fft::Engine::<Complex32, N, I, W, A>::new();
-    c.bench_with_input(
-        BenchmarkId::new(format!("{strategy_name}_{allocator_name}").as_str(), N),
-        &N,
-        |b, _| b.iter(|| boxed_engine.fft(black_box(&vec), &mut out_spec)),
-    );
-}
-
-fn generate_f32<const S: usize>() -> Arc<[f32; S]> {
-    let mut result: Box<[f32; S]> = vec![0.0f32; S].into_boxed_slice().try_into().unwrap();
-    let mut rnd = random::default(12304);
-    for val in result.iter_mut() {
-        *val = rnd.read_f64() as f32;
-    }
-    result.into()
-}
-
-fn generate_c32<const S: usize>() -> Arc<[Complex32; S]> {
-    let mut result: Box<[Complex32; S]> = vec![Complex32::default(); S]
+    let mut result: Box<[T; S]> = vec![Default::default(); S]
         .into_boxed_slice()
         .try_into()
         .unwrap();
-    let mut rnd = random::default(12304);
-    for val in result.iter_mut() {
-        *val = Complex32::new(rnd.read_f64() as f32, rnd.read_f64() as f32);
-    }
+    result.randomize(12304);
     result.into()
+}
+
+impl Randomizable<f32> for [f32] {
+    fn randomize(&mut self, seed: u64) {
+        let mut rnd = random::default(seed);
+        for val in self.iter_mut() {
+            *val = rnd.read_f64() as f32;
+        }
+    }
+}
+
+impl Randomizable<Complex32> for [Complex32] {
+    fn randomize(&mut self, seed: u64) {
+        let mut rnd = random::default(seed);
+        for val in self.iter_mut() {
+            *val = Complex32::new(rnd.read_f64() as f32, rnd.read_f64() as f32);
+        }
+    }
 }
 
 criterion_group!(benches, criterion_benchmark);
